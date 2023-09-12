b91d198eb1ac0a8afdba4973edf77c9b3477a6f3
rudendan <ruden.dan@gmail.com>
2023-09-12 23:56:51 +0300

    hw5 was added

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -1,4 +1,26 @@
 package phonebook;
 
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add(new Record("Denys", "380639999999"));
+        phoneBook.add(new Record("Max", "380963358877"));
+        phoneBook.add(new Record("Denys", "380634777388"));
+        phoneBook.add(new Record("Phillip", "38565480189"));
+
+        Record find = phoneBook.find("Denys");
+        if(find != null)
+            System.out.println(find.getName() + " " + find.getNumber() );
+
+        System.out.println();
+
+        List<Record> findAll = phoneBook.findAll("Denys");
+        for (Record record:findAll) {
+            System.out.println(record.getName() + " " + record.getNumber());
+        }
+    }
 }
--- src/phonebook/Record.java
+++ src/phonebook/Record.java
@@ -1,4 +1,20 @@
 package phonebook;
 
 public class Record {
+
+    private String name;
+    private String number;
+
+    public Record(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
 }
